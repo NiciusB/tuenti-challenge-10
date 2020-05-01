687af97a993ca7514eeb53524b5ef050b07cdb01
NiciusB <nuno@balbona.me>
2020-05-01 18:36:34 +0200

    14 start

--- 14/index.js
+++ 14/index.js
@@ -1,11 +1,16 @@
 const net = require('net');
+const fs = require('fs')
 
 const FINISHED_INTRO_LINE = 'AUTOJOIN CLUSTER ENABLED'
+const SELF_SERVER_ID = 9
 
 /*
+Intro tutorial:
+
 PREPARE {<positive_int>,9} -> <dest>
 ACCEPT {id: {<positive_int>,<positive_int>}, value: {servers: <server_list>, secret_owner: <positive_int>}} -> <dest>
 */
+
 /*
 Full round example:
 
@@ -16,94 +21,243 @@ ROUND 781: 6 -> ACCEPTED {servers: [1,2,3,4,5,6,8,9], secret_owner: 1}
 ROUND 781: 8 -> ACCEPTED {servers: [1,2,3,4,5,6,8,9], secret_owner: 1}
 ROUND 781: 9 -> LEARN {servers: [1,2,3,4,5,6,8,9], secret_owner: 1} (ROUND FINISHED)
 */
+
 /*
 All types format:
 
 ROUND 781: 8 -> ACCEPTED {servers: [1,2,3,4,5,6,8,9], secret_owner: 1}
+
 ROUND 746: 9 -> LEARN {servers: [1,2,3,5,7,8,9,10], secret_owner: 2} (ROUND FINISHED)
+
 ROUND 374: PREPARE {2,9} -> 4
+ROUND 658: 9 -> PREPARE {150,9}
+
 ROUND 348: 1 -> PROMISE {2,9} no_proposal
+ROUND 62: PROMISE {150,9} no_proposal -> 9
+
 ROUND 837: 1 -> PROMISE_DENIAL {2,9} {2,9}
+
+ROUND 332: ACCEPT {id: {150,9}, value: {servers: [1,2,3,4,5,6,7,9], secret_owner: 9}} -> 1
+
+ROUND 540: 1 -> ACCEPT_IGNORE {id: {150,9}, value: {servers: [1,2,3,5,6,7,8,9], secret_owner: 9}} (NOT TRUSTWORTHY TO CHANGE SECRET OWNER)
+
+ROUND 329: 27 -> ACCEPT_DENIAL {400,9} {401,26}
 */
 
+// debug.txt
+let debugTxt = ''
+fs.writeFileSync('debug.txt', '')
+setInterval(() => {
+    fs.appendFileSync('debug.txt', debugTxt)
+    debugTxt = ''
+}, 1000)
+
 async function main() {
-    const { sendLineToServer } = await openServerConnection(onServerResponse)
+    const { sendDataToServer } = await openServerConnection(onServerResponseLine)
 
     let isInIntro = true
-    function onServerResponse(serverData) {
-        serverData.split('\n').filter(Boolean).forEach(str => onServerResponseLine(str))
-    }
     function onServerResponseLine(line) {
+        debugTxt += line + '\n'
+
         if (isInIntro) {
             if (line === FINISHED_INTRO_LINE) isInIntro = false
             return
         }
 
         if (line.startsWith('ROUND ')) {
-            const type = /ROUND \d+:(?: \d+ ->)? (\w+)/.exec(line)[1]
+            try {
+                const type = /ROUND \d+:(?: \d+ ->)? (\w+)/.exec(line)[1]
+                let arrowNum
+                if (line.includes('->')) {
+                    const numFormat1 = /-> (\d+)/.exec(line)
+                    const numFormat2 = /(\d+) ->/.exec(line)
+                    arrowNum = parseInt((numFormat1 || numFormat2)[1])
+                }
 
-            if (type === 'ACCEPTED' || type === 'LEARN') {
-                const roundID = parseInt(/(\d+)\:/.exec(line)[1])
-                const num = parseInt(/(\d+)\ ->/.exec(line)[1])
-                const servers = /{servers: \[(.*?)\], secret/.exec(line)[1].split(',').map(num => parseInt(num))
-                const secretOwner = parseInt(/, secret_owner: (\d+)/.exec(line)[1])
-                const roundFinished = /\(ROUND FINISHED\)/.test(line)
-                const fn = type === 'ACCEPTED' ? onAccepted : onLearn
-                fn({ roundID, num, servers, secretOwner, roundFinished })
-                return
-            }
-            if (type === 'PREPARE') {
-                const roundID = parseInt(/(\d+)\:/.exec(line)[1])
-                const num = parseInt(/-> (\d+)/.exec(line)[1])
-                const ids = /{(\d+),(\d+)}/.exec(line)
-                onPrepare({ roundID, num, id1: parseInt(ids[1]), id2: parseInt(ids[2]) }) 
-                return
-            }
-            if (type === 'PROMISE') {
-                const roundID = parseInt(/(\d+)\:/.exec(line)[1])
-                const num = parseInt(/(\d+)\ ->/.exec(line)[1])
-                const ids = /{(\d+),(\d+)}/.exec(line)
-                onPromise({ roundID, num, id1: parseInt(ids[1]), id2: parseInt(ids[2]) }) 
-                return
-            }
-            if (type === 'PROMISE_DENIAL') {
-                const roundID = parseInt(/(\d+)\:/.exec(line)[1])
-                const num = parseInt(/(\d+)\ ->/.exec(line)[1])
-                const ids = /{(\d+),(\d+)} {(\d+),(\d+)}/.exec(line)
-                onPromiseDenial({ roundID, num, id1: parseInt(ids[1]), id2: parseInt(ids[2]), id3: parseInt(ids[3]), id4: parseInt(ids[4]) }) 
+                if (type === 'ACCEPTED' || type === 'LEARN') {
+                    const roundID = parseInt(/(\d+)\:/.exec(line)[1])
+                    const num = arrowNum
+                    const servers = /{servers: \[(.*?)\]/.exec(line)[1].split(',').map(num => parseInt(num))
+                    const secretOwner = parseInt(/secret_owner: (\d+)/.exec(line)[1])
+                    const roundFinished = /\(ROUND FINISHED\)/.test(line)
+                    const fn = type === 'ACCEPTED' ? onAccepted : onLearn
+                    fn({ roundID, num, servers, secretOwner, roundFinished })
+                    return
+                }
+                if (type === 'PREPARE') {
+                    const roundID = parseInt(/(\d+)\:/.exec(line)[1])
+                    const num = arrowNum
+                    const ids = /{(\d+),(\d+)}/.exec(line)
+                    onPrepare({ roundID, num, id1: parseInt(ids[1]), id2: parseInt(ids[2]) })
+                    return
+                }
+                if (type === 'PROMISE') {
+                    const roundID = parseInt(/(\d+)\:/.exec(line)[1])
+                    const promisingServer = arrowNum
+                    const ids = /{(\d+),(\d+)}/.exec(line)
+                    onPromise({ roundID, promisingServer, n: parseInt(ids[1]), preparingServer: parseInt(ids[2]) })
+                    return
+                }
+                if (type === 'PROMISE_DENIAL') {
+                    const roundID = parseInt(/(\d+)\:/.exec(line)[1])
+                    const num = arrowNum
+                    const ids = /{(\d+),(\d+)} {(\d+),(\d+)}/.exec(line)
+                    onPromiseDenial({ roundID, num, id1: parseInt(ids[1]), id2: parseInt(ids[2]), id3: parseInt(ids[3]), id4: parseInt(ids[4]) })
+                    return
+                }
+                if (type === 'ACCEPT') {
+                    const roundID = parseInt(/(\d+)\:/.exec(line)[1])
+                    const ids = /{(\d+),(\d+)}/.exec(line)
+                    const targetServer = arrowNum
+                    const servers = /{servers: \[(.*?)\]/.exec(line)[1].split(',').map(num => parseInt(num))
+                    const secretOwner = parseInt(/secret_owner: (\d+)/.exec(line)[1])
+                    onAccept({ roundID, n: parseInt(ids[1]), preparingServer: parseInt(ids[2]), targetServer, servers, secretOwner })
+                    return
+                }
+                if (type === 'ACCEPT_IGNORE') {
+                    console.error('ACCEPT_IGNORE', line)
+                    return
+                }
+                if (type === 'ACCEPT_DENIAL') {
+                    console.error('ACCEPT_DENIAL', line)
+                    return
+                }
+            } catch (error) {
+                console.error(line, error)
                 return
             }
 
-            throw new Error(`Unnown type ${type}: ${line}`)
+            console.error(`Unnown line: ${line}`)
         } else if (line.startsWith('BAD COMMAND IGNORED:')) {
-            throw new Error(line)
+            if (
+                line === 'BAD COMMAND IGNORED: Not enough promises' ||
+                line.startsWith('BAD COMMAND IGNORED: Invalid Dest Id ')
+            ) {
+                // ignore this error, since it's normal for it to happen sometimes. even tho maybe not? i'd have do research
+                return
+            }
+
+            console.error(line)
         } else {
-            throw new Error(`Unrecognised line: ${line}`)
+            console.error(`Unrecognised line: ${line}`)
         }
     }
 
+    // System status
+    const serversList = new Set()
+    let currentSecretOwner
+    function updateServersList(servers) {
+        serversList.clear()
+        servers.forEach(sID => serversList.add(sID))
+    }
+
+    // ACCEPTED
     function onAccepted({ roundID, num, servers, secretOwner, roundFinished }) {
-        console.log('ACCEPTED', { roundID, num, servers, secretOwner, roundFinished })
+        updateServersList(servers)
+        currentSecretOwner = secretOwner
+
+        //console.log('ACCEPTED', { roundID, num, servers, secretOwner, roundFinished })
     }
+
+    // LEARN
     function onLearn({ roundID, num, servers, secretOwner, roundFinished }) {
-        console.log('LEARN', { roundID, num, servers, secretOwner, roundFinished })
+        updateServersList(servers)
+        //console.log('LEARN', { roundID, num, servers, secretOwner, roundFinished })
     }
-    function onPromise({ roundID, num, id1, id2 }) {
-        console.log('PROMISE', { roundID, num, id1, id2 })
+
+    // ACCEPT
+    function onAccept({ roundID, n, preparingServer, targetServer, servers, secretOwner }) {
+        updateServersList(servers)
+        //console.log('ACCEPT', { roundID, n, preparingServer, targetServer, servers, secretOwner })
     }
+
+    // PROMISE
+    function setOnPromiseListener(callback) {
+        const callbackID = onPromiseListeners.push(callback) - 1
+        return () => onPromiseListeners.splice(callbackID, 1)
+    }
+    const onPromiseListeners = []
+    function onPromise({ roundID, promisingServer, n, preparingServer }) {
+        //console.log('PROMISE', { roundID, promisingServer, n, preparingServer })
+        onPromiseListeners.forEach(cb => cb({ roundID, promisingServer, n, preparingServer }))
+    }
+
+    // PROMISE_DENIAL
     function onPromiseDenial({ roundID, num, id1, id2, id3, id4 }) {
         console.log('PROMISE_DENIAL', { roundID, num, id1, id2, id3, id4 })
     }
+
+    // PREPARE
     function onPrepare({ roundID, num, id1, id2 }) {
-        console.log('PREPARE', { roundID, num, id1, id2 })
+        //console.log('PREPARE', { roundID, num, id1, id2 })
     }
 
-    function sendPrepare(min, dest) {
-        sendLineToServer(`PREPARE {${min},9} -> ${dest}`)
+    // Send line helpers
+    function sendPrepare({ servers, min }) {
+        const data = servers
+            .map(serverID => `PREPARE {${min},${SELF_SERVER_ID}} -> ${serverID}\n`)
+            .join('')
+        sendDataToServer(data)
     }
-    function sendAccept(id1, id2, servers, secretOwner, dest) {
-        sendLineToServer(`ACCEPT {id: {${id1},${id2}}, value: {servers: [${servers.join(',')}], secret_owner: ${secretOwner}}} -> ${dest}`)
+    function sendAccept({ servers, n, preparingServer, secretOwner }) {
+        const data = servers
+            .map(serverID => `ACCEPT {id: {${n},${preparingServer}}, value: {servers: [${servers.join(',')}], secret_owner: ${secretOwner}}} -> ${serverID}\n`)
+            .join('')
+        sendDataToServer(data)
     }
+
+    // Hack the system
+    let number = 0
+    function plotToBecomeOwner() {
+        number += 10
+        console.log('[plotToBecomeOwner]', serversList)
+        if (serversList.size <= 3 && Math.random() < 0.5) {
+            setTimeout(plotToBecomeOwner, 500)
+            return
+        }
+        startPromisePhase()
+
+        function startPromisePhase() {
+            const sentToServers = new Set(serversList)
+            const promisedServers = new Set()
+            sendPrepare({ min: number, servers: Array.from(sentToServers) })
+            const removePromiseListener = setOnPromiseListener(({ roundID, promisingServer, n, preparingServer }) => {
+                if (preparingServer !== SELF_SERVER_ID) return
+                promisedServers.add(promisingServer)
+                if (compareSet(promisedServers, sentToServers)) {
+                    clearTimeout(preparsePhaseTimeout)
+                    startAcceptPhase({ removePromiseListener, promisedServers })
+                }
+            })
+            const preparsePhaseTimeout = setTimeout(startAcceptPhase, 400, { removePromiseListener, promisedServers })
+        }
+        function startAcceptPhase({ removePromiseListener, promisedServers }) {
+            removePromiseListener()
+            setTimeout(plotToBecomeOwner, 10)
+            if (!compareSet(promisedServers, serversList)) return
+
+            const promisedServersArray = Array.from(promisedServers)
+            let secretOwner = currentSecretOwner
+
+            // Start Experiment: mess with servers list
+            if (promisedServersArray.length > 3) {
+                const indexToRemove = promisedServersArray
+                    .findIndex(serverID => serverID !== secretOwner && serverID !== SELF_SERVER_ID)
+                promisedServersArray.splice(indexToRemove, 1)
+            } else {
+                secretOwner = SELF_SERVER_ID
+            }
+            // End Experiment: mess with servers list
+
+            sendAccept({
+                n: number,
+                preparingServer: SELF_SERVER_ID,
+                secretOwner,
+                servers: promisedServersArray,
+            })
+        }
+    }
+    setTimeout(plotToBecomeOwner, 1000)
 }
 
 function openServerConnection(callback) {
@@ -111,18 +265,33 @@ function openServerConnection(callback) {
         var client = new net.Socket();
         client.connect(2092, '52.49.91.111', function () {
             console.debug('[openServerConnection] Connected');
-            const sendLineToServer = (string) => {
-                client.write(string + '\n')
+            const sendDataToServer = (string) => {
+                client.write(string)
             }
-            resolve({ sendLineToServer })
+            resolve({ sendDataToServer })
         });
+        let str = ''
         client.on('data', data => {
-            callback(data.toString())
+            str += data.toString()
+            while (str.includes('\n')) {
+                const split = str.split('\n')
+                str = split.pop()
+                split.forEach(line => callback(line))
+            }
         });
         client.on('close', function () {
             console.debug('[openServerConnection] Connection closed');
+        });
+        client.on('error', function (err) {
+            console.debug('[openServerConnection] Connection error', err);
         });
     })
 }
 
 main().catch(console.error)
+
+function compareSet(as, bs) {
+    if (as.size !== bs.size) return false;
+    for (var a of as) if (!bs.has(a)) return false;
+    return true;
+}
